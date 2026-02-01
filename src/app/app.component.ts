d40f30df64476b1e78df26a3bb35d09c17db6aa5
ruchajoshi2877-hub <ruchajoshi2877@gmail.com>
2026-02-01 23:47:41 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Ganesh';
+  title = 'Ganesh App';
+  result = 'Hello! This is my actual result 🎉';
 }
+
